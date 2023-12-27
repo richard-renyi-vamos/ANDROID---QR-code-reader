d388c5db76a934d16d03e62ad32985dca01fb410
Richard Renyi-Vamos <82152266+richard-renyi-vamos@users.noreply.github.com>
2023-12-27 13:11:23 +0100

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,78 @@
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start the camera
+                scannerView.startCamera();
+                scannerView.setResultHandler(this);
+            } else {
+                // Permission denied
+                Toast.makeText(this, "Camera permission required to scan QR codes", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            scannerView.setResultHandler(this);
+            scannerView.startCamera();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        // Handle the scanned result here
+        Log.d("QRCodeScanner", "Scanned Result: " + result.getText());
+
+        // Display the scanned result, for example, in a Toast
+        Toast.makeText(this, "Scanned Result: " + result.getText(), Toast.LENGTH_SHORT).show();
+
+        // Resume scanning after a short delay (optional)
+        scannerView.resumeCameraPreview(this);
+    }
+}
